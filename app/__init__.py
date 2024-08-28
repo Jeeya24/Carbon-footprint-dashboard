54b47081f92d3259d77940a2e7d9cf69b9fb0d11
Pritish Dugar <52480502+Pro509@users.noreply.github.com>
2024-08-29 02:46:26 +0700

    fix css

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ import os
 template_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'templates')
 
-app = Flask(__name__, template_folder=template_dir)
+static_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+static_dir = os.path.join(static_dir, 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 from app import routes
